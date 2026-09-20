6023396aed58709eb3ffb1d84d353b59f2d2d82b
NinjazGY2497 <gyagau@gmail.com>
2026-09-20 11:47:32 -0400

    Successful working (test) endpoints

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const http = require("node:http");
+import { createServer } from "node:http" // Going to use a import (newer) instead of require
 
-const HOSTNAME = "127.0.0.1";
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // 3000 is default, but check for PORT in env vars first
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("This is a Test");
+const server = createServer(async (request, response) => {
+    try {
+        const {method, url} = request;
+        
+        // Health Endpoint - a test endpoint to see if backend is running
+        if (method === "GET" && url === "/api/health") {
+            response.writeHead(200, {"Content-Type": "application/json"})
+            return response.end(JSON.stringify({status: "success", message: "Backend running!"}))
+        }
 
+        // Unknown Routes - Default to 404
+        response.writeHead(404, {"Content-Type": "application/json"})
+        return response.end(JSON.stringify({ error: "Route not found" }))
+        
+    } catch (err) {
+        console.log(err)
+        
+        response.writeHead(400, {"Content-Type": "application/json"})
+        return response.end(JSON.stringify({error: "Bad request/Invalid JSON"}))
+    }
+})
 
-});
-
-server.listen(PORT, HOSTNAME, () => {
-    console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
-});
+server.listen(PORT, () => {
+    console.log(`Backend server running on http://localhost:${PORT}`)
+})
